edd42f2fa47af55cbc398bf240d5936c7d09acee
pkiop <pkiop@naver.com>
2021-09-01 23:47:39 +0900

    Add testserver for obj get using http

---
+++ withReact/testServer/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.static('public'));
+app.get('/glb', (req, res) => {
+  res.send('glb');
+});
+
+app.listen(3030);
